9a221be8d6f64b34dc14e2d4c29cf042ecef8ed1
Hatice KATRANCI AKGÜL <76768140+haticekatranciakgul@users.noreply.github.com>
2024-04-17 23:35:15 +0300

    test buttons

---
+++ testing-react-app/src/components/Counter/Counter.test.js
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
+import Counter from './index'
+
+test("increase btn", () => {
+    render(<Counter/>);
+
+    const count = screen.getByText("0");
+    const increaseBtn = screen.getByText("Increase");
+
+    userEvent.click(increaseBtn);
+    expect(count).toHaveTextContent("1");
+
+
+});
+
+test("decrease btn", () => {
+    render(<Counter/>);
+
+    const count = screen.getByText("0");
+    const decreaseBtn = screen.getByText("Decrease");
+
+    userEvent.click(decreaseBtn);
+    expect(count).toHaveTextContent("-1");
+
+
+});
